0244c9302b72b3d0fd742f7332f71b45687907d2
vintagehsieh <vintagehsieh@gmail.com>
2022-12-26 11:43:46 +0800

    Services-IMemberRepository, Repositories-MemberRepository

---
+++ BookStore.Site/Models/Infrastructures/Repositories/MemberRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Site.Models.DTOs;
+using BookStore.Site.Models.Infrastructures.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Site.Models.Infrastructures.Repositories
+{
+    public class MemberRepository : IMemberRepository
+    {
+        public void Create(RegisterDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsExists(string account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookStore.Site/Models/Infrastructures/Services/IMemberRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Site.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Site.Models.Infrastructures.Services
+{
+    internal interface IMemberRepository
+    {
+        bool IsExists(string account);
+        void Create(RegisterDTO dto);
+
+    }
+}
