e863ad1c86014bc8a31c7d490460ee83621106fb
vintagehsieh <vintagehsieh@gmail.com>
2022-12-26 11:32:58 +0800

    DTOs-EncryptedPassword

--- BookStore.Site/Models/DTOs/RegisterDTO.cs
+++ BookStore.Site/Models/DTOs/RegisterDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStore.Site.Models.Infrastructures;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -13,6 +14,18 @@ namespace BookStore.Site.Models.DTOs
 
         public string Password { get; set; }
 
+        /// <summary>
+        /// 加密之後的密碼
+        /// </summary>
+        public string EncryptedPassword
+        {
+            get
+            {
+                string salt = "!@#$$DGTEGYT";
+                string result = HashUtility.TOSHA256(this.Password, salt);
+                return result;
+            }
+        }
         public string Email { get; set; }
 
         public string Name { get; set; }
--- Readme.txt
+++ Readme.txt
@@ -2,3 +2,5 @@
 [working on] add BookStore.Site MVC Project
 [woring on] Create BookStore db(bookstore_login, 123), Members table
 [working]
+
+-- ¹ê§@MemberService
