19a3047e7e36c54cfe4a380d11628c69bbbc22e3
vintagehsieh <vintagehsieh@gmail.com>
2022-12-26 13:56:13 +0800

    MembersController-Value Tuple

--- BookStore.Site/Controllers/MembersController.cs
+++ BookStore.Site/Controllers/MembersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BookStore.Site.Models.Infrastructures.Repositories;
+using BookStore.Site.Models.Infrastructures.Services;
+using BookStore.Site.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,35 @@ namespace BookStore.Site.Controllers
 {
     public class MembersController : Controller
     {
+        private IMemberRepository repository = new MemberRepository();
+
         // GET: Members
         public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var service = new MemberService(repository);
+
+            (bool IsSucess, string ErrorMessage) response = service.CreateNewMember(model.ToRequestDto());
+
+            if (response.IsSucess)
+            {
+                return View("RegisterConfirm");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, response.ErrorMessage);
+                return View(model);
+;            }
         }
     }
 }
---
+++ BookStore.Site/Models/Infrastructures/Services/MemberService.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Site.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Site.Models.Infrastructures.Services
+{
+    public class MemberService
+    {
+        private readonly IMemberRepository repository;
+
+        public MemberService(IMemberRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        //Value Tuple
+        public (bool IsSuccess, string ErrorMessage) CreateNewMember(RegisterDTO dto)
+        {
+            //todo 判斷各欄位是否正確
+
+            //判斷帳號是否存在
+            if (repository.IsExists(dto.Account))
+            {
+                return (false, "帳號已存在");
+            }
+    
+
+        #region 建立會員紀錄
+            //建立ConfirmCode
+            dto.ConfirmCode = Guid.NewGuid().ToString("N");
+            #endregion
+        }
+    }
+}
